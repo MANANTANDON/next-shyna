8245d5fe447881386617a817c8d7c7cd175ff318
mananTandon <manantandon@gmail.com>
2024-07-08 22:16:14 +0530

    Add files via upload

---
+++ src/components/MiddleCont.jsx
@@ -0,0 +1,70 @@
+import { Box, Container, Typography } from "@mui/material";
+import React from "react";
+
+export const MiddleCont = () => {
+  return (
+    <>
+      <Box sx={{ m: -1, p: 2 }}>
+        <Container
+          maxWidth="lg"
+          sx={{
+            bgcolor: "#50B49840",
+            borderRadius: "40px",
+            p: 1,
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <Typography
+            sx={{
+              textAlign: "center",
+              color: "#1A5319",
+              fontWeight: "bolder",
+              fontSize: "40px",
+              height: "60px",
+            }}
+          >
+            to 5348594E41
+          </Typography>
+          <Typography sx={{ color: "#1A5319", my: 40, fontSize: "50px" }}>
+            Hi Shy.....
+          </Typography>
+          <Typography sx={{ color: "#1A5319", fontSize: "30px", my: 100 }}>
+            Just Wanna Tell You ki now you have a space in the virtual world
+          </Typography>
+          <Typography sx={{ color: "#1A5319", fontSize: "30px", my: 100 }}>
+            ughhh, so there are 8.1 Billion People and un sb mein for instance
+            iff 500 million ludiyion ka naam SHYNA GUPTA hua toh vo ab yeh
+            domain nhi aquire kr skte..{" "}
+          </Typography>
+          <Typography sx={{ color: "#1A5319", fontSize: "30px", my: 100 }}>
+            AMAZING BAAT NHI HAI YEH!!! 🫠
+          </Typography>
+          <Typography sx={{ color: "#1A5319", fontSize: "30px", my: 100 }}>
+            Agr smj nhi ayi toh.... 🤔
+          </Typography>
+          <Typography sx={{ color: "#1A5319", fontSize: "30px", my: 100 }}>
+            bs yeh smj le ab aur koi bhi{" "}
+            <span style={{ borderBottom: "3px solid green" }}>
+              shynagupta.com
+            </span>{" "}
+            kbhi bhi aquire nhi kr pyega...
+          </Typography>
+          <Typography
+            sx={{
+              color: "#1A5319",
+              fontSize: "30px",
+              my: 100,
+              fontWeight: "bolder",
+            }}
+          >
+            ITS YOURS 4 LIFE
+          </Typography>
+          <Typography sx={{ color: "#1A5319", fontSize: "30px", my: 100 }}>
+            and also 5348594E41 == SHYNA in HexCode
+          </Typography>
+        </Container>
+      </Box>
+    </>
+  );
+};
