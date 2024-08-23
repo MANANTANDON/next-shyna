24e40f213162d819f4a9df32729a34771197072e
MANAN TANDON <manantandon@gmail.com>
2024-08-23 18:53:01 +0530

    intro changed...

--- src/components/TopContainer.jsx
+++ src/components/TopContainer.jsx
@@ -19,18 +19,37 @@ export const TopContainer = () => {
               justifyContent: "center",
             }}
           >
-            <Grid xs={12} md={6} item>
+            <Grid
+              xs={12}
+              md={6}
+              item
+              sx={{ display: { xs: "none", md: "block" } }}
+            >
               <Typography fontSize="30px">
                 <span style={{ fontWeight: "lighter" }}>Hey! </span>
                 <span style={{ fontWeight: "bolder" }}>I'm Shyna, </span>
                 <span style={{ fontWeight: "lighter" }}>A</span>
               </Typography>
-              <Typography fontSize="30px" fontWeight="bolder">
-                Convergence Journalist
+              <Typography fontSize="30px">
+                <span style={{ fontWeight: "lighter" }}>Based out of</span>
+                <span style={{ fontWeight: "bolder" }}> New Delhi, India.</span>
               </Typography>
               <Typography fontSize="30px">
-                <span style={{ fontWeight: "lighter" }}>Based in </span>
-                <span style={{ fontWeight: "bolder" }}>INDIA </span>
+                <span style={{ fontWeight: "lighter" }}>I write about </span>
+                <span style={{ fontWeight: "bolder" }}>
+                  politics, social issues and international{" "}
+                </span>
+                <span style={{ fontWeight: "lighter" }}>affairs.</span>
+              </Typography>
+              <Typography sx={{ my: 1 }}>
+                <span style={{ fontWeight: "lighter" }}>
+                  I can be reached at:{" "}
+                </span>
+                <span
+                  style={{ fontWeight: "bolder", textDecoration: "underline" }}
+                >
+                  shyna20012@gmail.com
+                </span>
               </Typography>
               <Typography fontSize="14px" sx={{ color: "#6087b5", my: 4 }}>
                 So you want to read about the latest world events? The little
@@ -87,9 +106,63 @@ export const TopContainer = () => {
                 />
               </Box>
             </Grid>
+            <Grid xs={12} md={6} item sx={{ display: { md: "none" } }}>
+              <Typography fontSize="25px">
+                <span style={{ fontWeight: "lighter" }}>Hey! </span>
+                <span style={{ fontWeight: "bolder" }}>I'm Shyna, </span>
+                <span style={{ fontWeight: "lighter" }}>A</span>
+              </Typography>
+              <Typography fontSize="25px">
+                <span style={{ fontWeight: "lighter" }}>Based out of</span>
+                <span style={{ fontWeight: "bolder" }}> New Delhi, India.</span>
+              </Typography>
+              <Typography fontSize="25px">
+                <span style={{ fontWeight: "lighter" }}>I write about </span>
+                <span style={{ fontWeight: "bolder" }}>
+                  politics, social issues and international{" "}
+                </span>
+                <span style={{ fontWeight: "lighter" }}>affairs.</span>
+              </Typography>
+              <Typography sx={{ my: 1 }}>
+                <span style={{ fontWeight: "lighter" }}>
+                  I can be reached at:{" "}
+                </span>
+                <span
+                  style={{ fontWeight: "bolder", textDecoration: "underline" }}
+                >
+                  shyna20012@gmail.com
+                </span>
+              </Typography>
+              <Typography fontSize="14px" sx={{ color: "#6087b5", my: 4 }}>
+                So you want to read about the latest world events? The little
+                nuggets of history we are still repeating, but are bored
+                scrolling the conventional wisdoms.. Perhaps you'd prefer some
+                poetic musings, strong opinions, and a few real- life tales
+                thrown in? Well, you are at the right place. Welcome to my
+                space!
+              </Typography>
+              <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+                <Link
+                  href="https://www.instagram.com/shyna.co/"
+                  className="links"
+                  target="_blank"
+                >
+                  <Instagram />
+                </Link>
+                <Link
+                  href="https://in.linkedin.com/in/shyna-gupta-053016210"
+                  target="_blank"
+                  className="links"
+                >
+                  <LinkedIn />
+                </Link>
+              </Box>
+            </Grid>
           </Grid>
         </Container>
       </Box>
     </>
   );
 };
+
+// I can be reached at: Shyna20012@gmail.com
