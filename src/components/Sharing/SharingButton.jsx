eacead094b6df69ab23d88c7d44075fff8f1607e
Manan Tandon <manantandon@Manans-MacBook-Air.local>
2025-01-27 21:22:55 +0530

    redesign complete...

--- src/components/Sharing/SharingButton.jsx
+++ src/components/Sharing/SharingButton.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { UIColor } from "@/constant";
 import {
   Facebook,
   IosShareRounded,
@@ -20,87 +21,88 @@ export const SharingButton = ({ slug, text }) => {
     }
   }, []);
 
-  if (!isClient || !navigator?.share || !isLaptop) {
-    return (
-      <>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            gap: 0.5,
-          }}
-        >
-          <a
-            href={`https://api.whatsapp.com/send?text=${text} ${slug}`}
-            style={{
-              padding: "10px",
-              borderRadius: "4px",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-            target="_blank"
-          >
-            <WhatsApp fontSize="small" sx={{ color: "#6087B5" }} />
-          </a>
-          <a
-            href={`https://www.linkedin.com/shareArticle?url=${slug}&title=${text}`}
-            style={{
-              padding: "10px",
-              borderRadius: "4px",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-            target="_blank"
-          >
-            <LinkedIn fontSize="small" sx={{ color: "#6087B5" }} />
-          </a>
-          <a
-            href={`https://www.facebook.com/sharer.php?u=${slug}`}
-            style={{
-              padding: "10px",
-              borderRadius: "4px",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-            target="_blank"
-          >
-            <Facebook fontSize="small" sx={{ color: "#6087B5" }} />
-          </a>
-          <a
-            href={`https://twitter.com/share?url=${slug}&text=${text}`}
-            style={{
-              padding: "10px",
-              borderRadius: "4px",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-            target="_blank"
-          >
-            <X fontSize="small" sx={{ color: "#6087B5" }} />
-          </a>
-        </Box>
-      </>
-    );
-  }
-
+  // if (!isClient || !navigator?.share || !isLaptop) {
   return (
-    <IconButton
-      onClick={() => {
-        navigator
-          .share({
-            title: text,
-            slug,
-          })
-          .then(() => console.log("Successful share"))
-          .catch(() => console.log("Error sharing"));
-      }}
-    >
-      <IosShareRounded fontSize="small" sx={{ color: "#000" }} />
-    </IconButton>
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          gap: 0.5,
+        }}
+      >
+        <a
+          href={`https://api.whatsapp.com/send?text=${text} ${slug}`}
+          style={{
+            padding: "10px",
+            borderRadius: "4px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          target="_blank"
+        >
+          <WhatsApp fontSize="small" sx={{ color: "#6087B5" }} />
+        </a>
+        <a
+          href={`https://www.linkedin.com/shareArticle?url=${slug}&title=${text}`}
+          style={{
+            padding: "10px",
+            borderRadius: "4px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          target="_blank"
+        >
+          <LinkedIn fontSize="small" sx={{ color: "#6087B5" }} />
+        </a>
+        <a
+          href={`https://www.facebook.com/sharer.php?u=${slug}`}
+          style={{
+            padding: "10px",
+            borderRadius: "4px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          target="_blank"
+        >
+          <Facebook fontSize="small" sx={{ color: "#6087B5" }} />
+        </a>
+        <a
+          href={`https://twitter.com/share?url=${slug}&text=${text}`}
+          style={{
+            padding: "10px",
+            borderRadius: "4px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          target="_blank"
+        >
+          <X fontSize="small" sx={{ color: "#6087B5" }} />
+        </a>
+      </Box>
+    </>
   );
+  // }
+
+  // return (
+  //   <IconButton
+  //     onClick={() => {
+  //       navigator
+  //         .share({
+  //           title: text,
+  //           slug,
+  //         })
+  //         .then(() => console.log("Successful share"))
+  //         .catch(() => console.log("Error sharing"));
+  //     }}
+  //     sx={{ color: UIColor }}
+  //   >
+  //     <IosShareRounded fontSize="small" sx={{ color: "#000" }} />
+  //   </IconButton>
+  // );
 };
