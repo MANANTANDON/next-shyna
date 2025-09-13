42a281f01c78041dc5760263f8a6df2c6c401397
mananTandon <manantandon@gmail.com>
2025-09-13 12:42:00 +0530

    gtag integration...

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,42 @@
 import "@/styles/globals.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag("config", "G-JSJC4NY9T7", {
+        page_path: url,
+      });
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      {/* Google Analytics Script */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-JSJC4NY9T7"
+        strategy="afterInteractive"
+      />
+      <Script id="ga-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-JSJC4NY9T7', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
