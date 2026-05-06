04d7acfc30501c41cc7ce622a1b084396997dbc6
mananTandon <manantandon@gmail.com>
2026-05-06 15:34:10 +0530

    /new page deleted...

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -89,10 +89,10 @@ export default function Home({ articlesRes }) {
         />
       </Head>
       <Layout>
-        {/* <TopContainer articles={articlesRes} /> */}
-        {/* <SecondContainer articles={articlesRes} />
+        <TopContainer articles={articlesRes} />
+        <SecondContainer articles={articlesRes} />
         <ThirdContainer articles={articlesRes} />
-        <FourthContainer articles={articlesRes} /> */}
+        <FourthContainer articles={articlesRes} />
       </Layout>
     </>
   );
